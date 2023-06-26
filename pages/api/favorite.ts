4dc7e57a976644e0f4b5bd30e2e5b840bff6ddf2
alainfranck <franckalain.dev@gmail.com>
2023-06-26 21:31:09 +0100

    first commit

---
+++ pages/api/favorite.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { without } from "lodash";
+import prismadb from '@/lib/prismadb'
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        if (req.method === 'POST') {
+            const { currentUser } = 
+        }
+
+    } catch (error) {
+        console.log(error)
+        return res.status(405).end()
+    }
+}
